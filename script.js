7b4e4a9375dce29e67d29657066f495870afb89a
prash4 <prashanthgowdars555@gmail.com>
2024-12-08 15:13:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $("#registrationForm").on("submit", function (event) {
+      let isValid = true;
+  
+      $("input, textarea").each(function () {
+        if ($(this).val() === "") {
+          isValid = false;
+          alert("Please fill out all fields.");
+          return false;
+        }
+      });
+  
+      return isValid;
+    });
+  });
+  
